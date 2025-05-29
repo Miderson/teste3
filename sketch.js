cd460f557b5e44d6432ff4aa4a1cc04b6ecb740e
Miderson Santana <103211766+Miderson@users.noreply.github.com>
2025-05-29 08:21:09 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(100,52,100)
+  circle(200, 200, 150);
+  fill(0,0,0)
+  circle(80, 100, 50);
+  fill(255,255,250)
+  circle(180, 100, 50);
+  fill(0,255,0)
+  triangle(200, 180, 250, 180, 70,70 );
+  
+}
+
